4e08fb4179e73a54590c1961eae698e4ba7230c8
Latos <thelatos.06@gmail.com>
2026-01-17 02:15:53 +0800

    重构

--- client.c
+++ client.c
@@ -1,13 +1,18 @@
 #include "client.h"
-#include <unistd.h>
+#include "spawn.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/wait.h>
 #include <termios.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
+#include <unistd.h>
 
+volatile sig_atomic_t sigwinch_pending,
+    sigchld_pending = 0; // C 语言唯一保证信号读写安全的类型
 static const state_transition table[] = {
     {ST_BOOT, EV_ENABLE_RAW_MODE, ST_RUNNING, act_enable_raw_mode},
     {ST_RUNNING, EV_WINCH, ST_RUNNING, act_resize},
@@ -22,86 +27,175 @@ static const state_transition table[] = {
 
 #define NTRANS (sizeof(table) / sizeof(table[0]))
 
-void dispatch_event(struct client *c, client_event ev)
-{
-    for (size_t i = 0; i < NTRANS; i++)
-    {
-        if (table[i].state == c->state && table[i].event == ev)
-        {
-            if (table[i].action)
-            {
-                table[i].action(c, ev);
-            }
+void dispatch_event(struct client *c, client_event ev) {
+  for (size_t i = 0; i < NTRANS; i++) {
+    if (table[i].state == c->state && table[i].event == ev) {
+      if (table[i].action) {
+        table[i].action(c, ev);
+      }
 
-            c->state = table[i].next;
-            return;
-        }
+      c->state = table[i].next;
+      return;
     }
-    fprintf(stderr,
-            "[FSM] unhandled event %d in state %d\n",
-            ev, c->state);
+  }
+  fprintf(stderr, "[FSM] unhandled event %d in state %d\n", ev, c->state);
 }
 
-void act_resize(struct client *c, client_event ev)
-{
-    // 设置终端尺寸
-    if (ioctl(STDIN_FILENO, TIOCGWINSZ, &(c->ws)) == -1)
-    {
-        return;
-    }
-    ioctl(c->master_fd, TIOCSWINSZ, &(c->ws));
+void act_resize(struct client *c, client_event ev) {
+  // 设置终端尺寸
+  if (ioctl(STDIN_FILENO, TIOCGWINSZ, &(c->ws)) == -1) {
     return;
+  }
+  ioctl(c->master_fd, TIOCSWINSZ, &(c->ws));
+  return;
 }
 
-void act_child_exit(struct client *c, client_event ev)
-{
-    c->child_exited = 1;
-    char msg[100] = {0};
-    snprintf(msg, sizeof(msg), "Child exited with PID: %d\n", c->slave_pid);
-    write(STDOUT_FILENO, msg, strlen(msg));
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &(c->orig_termios));
+void act_child_exit(struct client *c, client_event ev) {
+  c->child_exited = 1;
+  char msg[100] = {0};
+  snprintf(msg, sizeof(msg), "Child exited with PID: %d\n", c->slave_pid);
+  write(STDOUT_FILENO, msg, strlen(msg));
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &(c->orig_termios));
 }
 
-void act_enable_raw_mode(struct client *c, client_event ev)
-{
-    // 原始终端切换至 raw 模式
-    tcgetattr(STDIN_FILENO, &(c->raw));
-    c->raw.c_lflag &= ~(ECHO | ICANON | ISIG); // 关掉回显/ 立即读取 / 禁用SIGINT
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &(c->raw));
+void act_enable_raw_mode(struct client *c, client_event ev) {
+  // 原始终端切换至 raw 模式
+  tcgetattr(STDIN_FILENO, &(c->raw));
+  c->raw.c_lflag &= ~(ECHO | ICANON | ISIG); // 关掉回显/ 立即读取 / 禁用SIGINT
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &(c->raw));
 }
 
-void act_pty_read(struct client *c, client_event ev)
-{
-    char buff[4096];
-    ssize_t n = read(c->master_fd, buff, sizeof(buff));
-    if (n <= 0)
-    {
-        dispatch_event(c, EV_EOF_PTY);
-        return;
+void act_pty_read(struct client *c, client_event ev) {
+  char buff[4096];
+  ssize_t n = read(c->master_fd, buff, sizeof(buff));
+  if (n <= 0) {
+    dispatch_event(c, EV_EOF_PTY);
+    return;
+  }
+  write(STDOUT_FILENO, buff, n);
+}
+
+void act_stdin_read(struct client *c, client_event ev) {
+  char buff[4096];
+  ssize_t n = read(STDIN_FILENO, buff, sizeof(buff));
+  if (n <= 0) {
+    dispatch_event(c, EV_EOF_STDIN);
+    return;
+  }
+  write(c->master_fd, buff, n);
+}
+
+/*
+ * Signal handlers are async and may interrupt execution at arbitrary points.
+ * Only set flags here; handle logic in main loop.
+ */
+void signal_handler(int sig) {
+  extern struct client client;
+  int status;
+  int ret;
+  switch (sig) {
+  case SIGWINCH:
+    sigwinch_pending = 1;
+    break;
+  // 回收子进程
+  case SIGCHLD:
+    ret = waitpid(client.slave_pid, &status, WNOHANG);
+    if (ret > 0) {
+      sigchld_pending = 1;
     }
-    write(STDOUT_FILENO, buff, n);
+    break;
+  }
 }
 
-void act_stdin_read(struct client *c, client_event ev)
-{
-    char buff[4096];
-    ssize_t n = read(STDIN_FILENO, buff, sizeof(buff));
-    if (n <= 0)
-    {
-        dispatch_event(c, EV_EOF_STDIN);
-        return;
+void client_init(struct client *c) {
+  c->state = ST_BOOT;
+  c->master_fd = -1;
+  c->slave_fd = -1;
+  c->slave_pid = -1;
+  c->child_exited = 0;
+
+  tcgetattr(STDIN_FILENO, &(c->orig_termios));
+  ioctl(STDIN_FILENO, TIOCGWINSZ, &(c->ws));
+}
+
+void client_loop(struct client *c){
+    while (1) {
+    if (c->child_exited)
+      break;
+    fd_set rfds;
+
+    // 输入和输出
+    int maxfd;
+    FD_ZERO(&rfds);
+    FD_SET(c->master_fd, &rfds);
+    FD_SET(STDIN_FILENO, &rfds);
+
+    maxfd = c->master_fd > STDIN_FILENO ? c->master_fd : STDIN_FILENO;
+
+    int select_ok = 1;
+    if (select(maxfd + 1, &rfds, NULL, NULL, NULL) < 0) {
+      // 防止收到信号后中断 fd
+      if (errno != EINTR) {
+        dispatch_event(c, EV_INTERRUPT);
+        perror("select");
+        break;
+      }
+      // fd 检查完毕
+      select_ok = 0;
     }
-    write(c->master_fd, buff, n);
+
+    if (sigwinch_pending) {
+      sigwinch_pending = 0;
+      dispatch_event(c, EV_WINCH);
+    }
+
+    if (sigchld_pending) {
+      sigchld_pending = 0;
+      dispatch_event(c, EV_CHLD_EXIT);
+    }
+
+    // 只有 select 成功时才检查 fd
+    if (select_ok) {
+      if (FD_ISSET(c->master_fd, &rfds)) {
+        dispatch_event(c, EV_PTY_READ);
+      }
+
+      if (FD_ISSET(STDIN_FILENO, &rfds)) {
+        dispatch_event(c, EV_STDIN_READ);
+      }
+    }
+  }
+
 }
+int client_main(struct client *c) {
+  c->master_fd = posix_openpt(O_RDWR);
+  if (c->master_fd == -1) {
+    perror("posix_openpt");
+    return -1;
+  }
+  // 解锁 slave 设备
+  grantpt(c->master_fd);
+  unlockpt(c->master_fd);
+  c->slave_name = ptsname(c->master_fd);
+  c->slave_fd = open(c->slave_name, O_RDWR);
+  if (ioctl(STDIN_FILENO, TIOCGWINSZ, &c->ws) == -1) {
+    return -1;
+  }
+  ioctl(c->slave_fd, TIOCSWINSZ, &c->ws);
+  c->slave_pid = spawn_child(c);
+  if (c->slave_pid < 0) {
+    return -1;
+  }
+  // 终端窗口尺寸更新
+  struct sigaction sa;
+  sa.sa_handler = signal_handler;
+  sa.sa_flags = SA_RESTART;
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGWINCH, &sa, NULL);
+  sigaction(SIGCHLD, &sa, NULL);
 
-void client_init(struct client *c)
-{
-    c->state = ST_BOOT;
-    c->master_fd = -1;
-    c->slave_fd = -1;
-    c->slave_pid = -1;
-    c->child_exited = 0;
-
-    tcgetattr(STDIN_FILENO, &(c->orig_termios));
-    ioctl(STDIN_FILENO, TIOCGWINSZ, &(c->ws));
+  dispatch_event(c, EV_ENABLE_RAW_MODE);
+  printf("Spawned child process with PID: %d\n", c->slave_pid);
+  client_loop(c);
+  return 0;
 }
--- client.h
+++ client.h
@@ -1,51 +1,48 @@
 // client.h
-#include <sys/types.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
-typedef enum
-{
-    ST_BOOT,
-    ST_RUNNING,
-    ST_RESIZING,
-    ST_EXITING,
+typedef enum {
+  ST_BOOT,
+  ST_RUNNING,
+  ST_RESIZING,
+  ST_EXITING,
 } client_state;
 
-typedef enum
-{
-    EV_STDIN_READ,
-    EV_PTY_READ,
-    EV_WINCH,
-    EV_CHLD_EXIT,
-    EV_INTERRUPT,
-    EV_EOF_STDIN,
-    EV_EOF_PTY,
-    EV_ENABLE_RAW_MODE
+typedef enum {
+  EV_STDIN_READ,
+  EV_PTY_READ,
+  EV_WINCH,
+  EV_CHLD_EXIT,
+  EV_INTERRUPT,
+  EV_EOF_STDIN,
+  EV_EOF_PTY,
+  EV_ENABLE_RAW_MODE
 } client_event;
 
-struct client
-{
-    client_state state;
-    int master_fd;
-    int slave_fd;
-    pid_t slave_pid;
-    struct winsize ws;
-    struct termios orig_termios;
-    int child_exited;
-    struct termios raw;
-    char *slave_name;
-    struct environ		*environ;
+struct client {
+  client_state state;
+  int master_fd;
+  int slave_fd;
+  pid_t slave_pid;
+  struct winsize ws;
+  struct termios orig_termios;
+  int child_exited;
+  struct termios raw;
+  char *slave_name;
+  struct environ *environ;
 };
 
 typedef void (*action_fn)(struct client *c, client_event ev);
-typedef struct
-{
-    client_state state;
-    client_event event;
-    client_state next;
-    action_fn action;
+typedef struct {
+  client_state state;
+  client_event event;
+  client_state next;
+  action_fn action;
 } state_transition;
 
 void client_init(struct client *c);
+int client_main(struct client *c);
 void dispatch_event(struct client *c, client_event ev);
 void act_resize(struct client *C, client_event ev);
 void act_child_exit(struct client *C, client_event ev);
