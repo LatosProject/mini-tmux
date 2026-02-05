23f63874fcdb93b039d954906ca2dff1f4d56b31
latos <thelatos.06@gmail.com>
2026-02-05 23:08:45 +0800

    update

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define MINI_TMUX_VERSION "0.4.1"
+#define MINI_TMUX_VERSION "0.4.2"
 #define MINI_TMUX_VERSION_STRING "mini-tmux v" MINI_TMUX_VERSION
 
 #endif /* VERSION_H */
